7aecdfeb801fbc7485d52a9aa0b53e578132782f
MinegishiYunosuke <y.minegishi0924@gmail.com>
2021-02-18 00:08:28 +0900

    Setting of rails g

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,12 @@ module RunteqNormal
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.generators do |g|
+      g.assets false # CSS, JavaScriptファイル生成せず
+      g.skip_routes false # trueならroutes.rb変更せず、falseなら通常通り変更
+      g.helper false 
+      g.test_framework false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
