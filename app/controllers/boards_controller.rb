b66b079c42e5b51288d45d962f088f0877cd1058
MinegishiYunosuke <y.minegishi0924@gmail.com>
2021-02-21 17:27:02 +0900

    Finish

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -8,4 +8,20 @@ class BoardsController < ApplicationController
     @board = Board.new
   end
 
+  def create
+    @board = Board.new(board_params)
+    @board.user_id = current_user.id
+    if @board.save
+      redirect_to boards_path, success: (t '.success')
+    else
+      flash.now[:danger] = t '.fail'
+      render :new
+    end
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title, :body)
+  end
 end
