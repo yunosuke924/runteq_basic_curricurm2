f4afb27bb2cce3559ed2132eba10a57fd8280a0d
MinegishiYunosuke <y.minegishi0924@gmail.com>
2021-03-01 04:33:08 +0900

    refactoring

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,19 @@
 class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.build(comment_params)
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to board_path }
-        format.js
-      else
-        format.html { render :new }
-        format.js { render :errors }
-      end
+    @comment.save
       #   # redirect_back(fallback_location: root_path)
       #   redirect_to board_path(comment.board), success: (t '.success') # 作成したコメントの対象の投稿の詳細画面に遷移
       #   # flash[:success] = t '.success'
       # else
       #   redirect_to board_path(comment.board), danger: (t '.fail')
       # end
-    end
   end
 
   def destroy
+    #@comment = current_user.comments.find(params[:id])
     @comment = Comment.find(params[:id])
-    @comment.destroy
+    @comment.destroy!
     # render root_path
     # redirect_back(fallback_location: root_path)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,9 @@ Rails.application.routes.draw do
   resources :users, only: %i[new create]
   resources :boards, only: %i[index new create show edit update destroy] do
     resources :comments, only: %i[create destroy], shallow: true
-
     collection do
       get :bookmarks
     end
   end
-
   resources :bookmarks, only: %i[create destroy]
 end
