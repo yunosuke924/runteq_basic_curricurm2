04428c7f1c1da9e183cb63ab48dbe947c5f6dc13
MinegishiYunosuke <y.minegishi0924@gmail.com>
2021-03-03 02:14:30 +0900

    fix

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ require 'sprockets/railtie'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
 module RunteqNormal
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
