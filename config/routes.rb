bd66472aa1c3d0ade33a75b1f6c5dd222a0b1383
MinegishiYunosuke <y.minegishi0924@gmail.com>
2021-03-03 02:37:49 +0900

    bundle fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' # if Rails.env.development?
+  
   get 'password_resets/create'
   get 'password_resets/edit'
   get 'password_resets/update'
@@ -19,5 +21,4 @@ Rails.application.routes.draw do
   resource :profile, only: %i[show edit update]
   resources :password_resets, only: %i[create edit update new]
 
-  mount LetterOpenerWeb::Engine, at: '/letter_opener' # if Rails.env.development?
 end
