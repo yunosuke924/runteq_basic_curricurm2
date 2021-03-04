3a4b0900d2808fb8232632fe5bfc03c5bcad3ea7
MinegishiYunosuke <y.minegishi0924@gmail.com>
2021-03-04 16:36:35 +0900

    set before_action at BaseController

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,12 @@
 class Admin::BaseController < ApplicationController
   skip_before_action :require_login
+
+  def require_admin
+    user = User.find_by(email: params[:email])
+
+    if user.general?
+        redirect_to root_path, success: '権限がありません'
+    end
+  end
+    
 end
--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -1,19 +1,16 @@
 class Admin::UserSessionsController < Admin::BaseController
   layout 'admin/layouts/admin_login'
+  before_action :require_admin, only: [:create]
   # 新規ログイン画面
   def new; end
 
   def create
     @user = login(params[:email], params[:password])
-    if @user.admin?
       if @user
         redirect_to admin_root_path, success: 'ログインしました'
       else
         render :new, danger: 'ログインに失敗しました'
       end
-    else
-      redirect_to root_path, success: 'ログインしました'
-    end
   end
 
   def destroy
