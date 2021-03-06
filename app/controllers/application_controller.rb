ea8a978d67d6b0d6f4326147a516aa6038dea62d
MinegishiYunosuke <y.minegishi0924@gmail.com>
2021-03-07 05:42:44 +0900

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,23 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
   before_action :require_login
 
-  rescue_from StandardError, with: :handle_500 unless Rails.env.development?
-  rescue_from ActiveRecord::RecordNotFound, with: :handle_404 unless Rails.env.development?
+  rescue_from StandardError, with: :handle500 unless Rails.env.development?
+  rescue_from ActiveRecord::RecordNotFound, with: :handle404 unless Rails.env.development?
 
-  def handle_404
+  def handle404
     # unless Rails.env.development?
-    render file: Rails.root.join('public', '404.html'),
+    render file: Rails.root.join('publc/404'),
            status: :not_found, layout: false
   end
 
-  def handle_500(e)
+  def handle500(error)
     # unless Rails.env.development?
-    logger.debug(e.backtrace.join("\n"))
+    logger.debug(error.backtrace.join("\n"))
     logger.info('logger.info')
-    logger.info(e)
+    logger.info(error)
     ExceptionNotifier.notify_exception(e, env: request.env,
                                           data: { message: 'error' })
-    render file: Rails.root.join('public', '500.html'),
+    render file: Rails.root.join('public/500'),
            status: :internal_server_error, layout: false
   end
 
